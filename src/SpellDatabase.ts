336210ca1e5dcf812e7d5682f1fc1c996d443f04
Sebastian Jørgensen <sj@invokit.dk>
2019-11-30 22:05:44 +0100

    Use browsers caching for spell data requests

--- src/SpellDatabase.ts
+++ src/SpellDatabase.ts
@@ -36,11 +36,44 @@ type Spell = {
     spellResistance: string | null;
 };
 
-async function downloadInitialData(): Promise<Spell[]> {
-    const response = await fetch(INITIAL_DATA_URL);
+var _db: IDBDatabase | null = null;
+
+function getDb() {
+    if (_db != null) {
+        return _db;
+    } else {
+        throw new Error("Database not initialised.");
+    }
+}
+
+function putSpells(db: IDBDatabase, spells: Spell[]): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+        const tr = db.transaction(DB_SPELLS_STORE_NAME, "readwrite");
+        tr.onerror = (err) => reject(err);
+    
+        const os = tr.objectStore(DB_SPELLS_STORE_NAME);
+        await Promise.all(spells.map(s => {
+            return new Promise<void>((resolve) => {
+                const putRequest = os.put(s);
+                putRequest.onsuccess = e => resolve();
+            });
+        }));
+
+        resolve();
+    });
+}
+
+async function fetchSpells(db: IDBDatabase, ignoreCache: boolean) {
+    const headers: any = {};
+    if (ignoreCache) {
+        headers["Cache-Control"] = "no-cache";
+    }
+
+    const response = await fetch(INITIAL_DATA_URL, { headers });
+
     if (response.ok) {
         const spells: Array<any> = await response.json();
-        return spells.map(s => {
+        await putSpells(db, spells.map(s => {
             if (s.classLevels) {
                 s.classLevels = s.classLevels.filter((v: any) => v != null).map((v: any) => {
                     return [v.class, v.level];
@@ -52,17 +85,17 @@ async function downloadInitialData(): Promise<Spell[]> {
                 });
             }
             return s as Spell;
-        });
+        }));
+    } else if (response.status === 304) {
+        // Not Modified
     } else {
         throw new Error(response.statusText);
     }
 }
 
-var _db: IDBDatabase | null = null;
-
 export async function initialise() : Promise<void> {
     return new Promise<void>((resolve, reject) => {
-        let upgradePromise: (Promise<void> | undefined) = undefined;
+        let upgraded = false;
 
         function onBlocked(this: IDBOpenDBRequest, e: Event): any {
             reject(new Error("Database initialisation was blocked."));
@@ -72,33 +105,20 @@ export async function initialise() : Promise<void> {
             reject(new Error("Error initialising database."));
         }
         
-        function onSuccess(this: IDBRequest<IDBDatabase>, e: Event): any {
-            _db = this.result;
-            resolve(upgradePromise);
+        async function onSuccess(this: IDBRequest<IDBDatabase>, e: Event): Promise<any> {
+            await fetchSpells(this.result, upgraded);
+            resolve();
         }
         
         function onUpgradeNeeded(this: IDBOpenDBRequest, e: IDBVersionChangeEvent) {
-            upgradePromise = new Promise((resolve, reject) => {
-                // @ts-ignore result property does not exist
-                const db: IDBDatabase = e.target.result;
+            // @ts-ignore result property does not exist
+            const db: IDBDatabase = e.target.result;
 
-                const objectStore = db.createObjectStore(DB_SPELLS_STORE_NAME, { keyPath: "name" });
-                objectStore.createIndex(DB_INDEX_CLASSANDLEVEL, "classLevels", { multiEntry: true });
-                objectStore.createIndex(DB_INDEX_DOMAINANDLEVEL, "domainLevels", { multiEntry: true });
+            const objectStore = db.createObjectStore(DB_SPELLS_STORE_NAME, { keyPath: "name" });
+            objectStore.createIndex(DB_INDEX_CLASSANDLEVEL, "classLevels", { multiEntry: true });
+            objectStore.createIndex(DB_INDEX_DOMAINANDLEVEL, "domainLevels", { multiEntry: true });
 
-                objectStore.transaction.oncomplete = async e => {
-                    // Download spells and add them to the object store.
-                    const spells: Spell[] = await downloadInitialData();
-
-                    const tr = db.transaction(DB_SPELLS_STORE_NAME, "readwrite");
-                    tr.onerror = (err) => reject(err);
-
-                    const os = tr.objectStore(DB_SPELLS_STORE_NAME);
-                    spells.forEach(s => os.put(s));
-
-                    resolve();
-                }; 
-            });
+            upgraded = true;
         }
         
         if (_db != null) {
@@ -111,14 +131,6 @@ export async function initialise() : Promise<void> {
         dbOpenRequest.onsuccess = onSuccess;
         dbOpenRequest.onupgradeneeded = onUpgradeNeeded;    
     });
-}
-
-function getDb() {
-    if (_db != null) {
-        return _db;
-    } else {
-        throw new Error("Database not initialised.");
-    }
 }
 
 function readFromObjectStore<T>(objectStoreName: string, useStoreFn: (store: IDBObjectStore) => Promise<T>): Promise<T> {
